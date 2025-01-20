2b2f5545b1f5cc026b47db643c235511353a454a
Gaffer Fitch <gaffer.fitch@gmail.com>
2025-01-20 14:30:33 -0500

    Moving scratch to its own repo

--- scratch/bin/draft.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import json
-import github
-
-client = github.QL("unifist", "unum")
-
-print(client.draft("Dude", "Sweet"))
--- scratch/lib/outreach.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def config():
-
-    with open("/opt/service/config/outreach.json", "r") as outreach_file:
-        return json.load(outreach_file)
